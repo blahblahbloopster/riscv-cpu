700881d06b2465ed8fd1a96efb93016985888f73
Quinn Yockey <qyockey@tutanota.de>
2024-05-25 17:09:56 -0700

    add register array with two load busses and one store bus

--- register.sv
+++ register.sv
@@ -2,9 +2,11 @@ module register (
     input clk,
     input reset_n,
     input [31:0] data,
-    input load,
-    input enable_n,
-    inout [31:0] out
+    input store,
+    input enable_a,
+    input enable_b,
+    inout [31:0] a_out,
+    inout [31:0] b_out
 );
 
     reg [31:0] value;
@@ -12,12 +14,13 @@ module register (
     always @(posedge clk) begin
         if (!reset_n) begin
             value <= 0;
-        end else if (load) begin
+        end else if (store) begin
             value <= data;
         end
     end
 
-    assign out = (enable_n) ? 32'bz : value;
+    assign a_out = (enable_a) ? value : 32'bz;
+    assign b_out = (enable_b) ? value : 32'bz;
 
 endmodule
 
