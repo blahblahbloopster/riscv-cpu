d09e94c2b3186ae7b7111098d379a56da751f683
Quinn Yockey <qyockey@tutanota.de>
2024-06-04 20:40:27 -0700

    only handle relative branches, given instruction

--- branch.sv
+++ branch.sv
@@ -1,33 +1,57 @@
+`define XLEN 32
+`define REG_SELECT_LEN 5
+`define BUS_HI_Z `XLEN'hzzzzzzzz
+`define SELECT_HI_Z `REG_SELECT_LEN'bzzzzz
+
 module branch (
-    input logic clk,
-    input logic [31:0] register_source_1,
-    input logic [31:0] register_source_2,
-    input logic [31:0] program_counter,
-    input logic [31:0] offset,
-    input logic [2:0] opcode,
-    input logic enable_n,
-    inout logic [31:0] new_program_counter
+    input  logic             clk,
+    input  logic             enable_n,
+
+    input  logic [`XLEN-1:0] instruction,
+    input  logic [`XLEN-1:0] program_counter,
+
+    output logic [`REG_SELECT_LEN-1:0] register_1,
+    output logic [`REG_SELECT_LEN-1:0] register_2,
+    input  logic [`XLEN-1:0]           register_data_1,
+    input  logic [`XLEN-1:0]           register_data_2,
+
+    output logic [`XLEN-1:0] compare_data_1,
+    output logic [`XLEN-1:0] compare_data_2,
+    input  logic [`XLEN-1:0] alu_out,
+
+    output logic             load_new_program_counter;
+    output logic [`XLEN-1:0] new_program_counter,
 );
 
     logic compare_success;
     logic jump;
+    logic [2:0] alu_op;
 
     always @(posedge clk) begin
-        // big optimization later
-        case (opcode[2:0])
-            3'b000: compare_success <= register_source_1 == register_source_2;
-            3'b001: compare_success <= register_source_1 != register_source_2;
-            3'b100: compare_success <= $signed(register_source_1)
-                                     < $signed(register_source_2);
-            3'b100: compare_success <= $signed(register_source_1)
-                                    >= $signed(register_source_2);
-            3'b110: compare_success <= register_source_1 <  register_source_2;
-            3'b111: compare_success <= register_source_1 >= register_source_2;
-        endcase
+        if (!enable_n) begin
+            register_1 <= instruction[12:8];
+            register_2 <= instruction[17:13];
+        end else begin
+            register_1 <= `HI_Z;
+            register_2 <= `HI_Z;
+        end
     end
 
-    assign jump = compare_success & ~enable_n;
-    assign new_program_counter = program_counter + (jump ? offset : 4);
+    always_comb begin
+        if (!enable_n) begin
+            compare_data_1 = register_data_1;
+            compare_data_2 = register_data_2;
+
+            // uhh black magic fuckery, hope i dont have to debug this
+            alu_op = {~instruction[14], instruction[14], instruction[13]};
+            load_new_program_counter = |alu_out ^ instruction[12];
+
+        end else begin
+            compare_data_1 = `BUS_HI_Z;
+            compare_data_2 = `BUS_HI_Z;
+            load_new_program_counter = 1'bz;
+        end
+    end
 
 endmodule
 
