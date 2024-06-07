1de47d7845c8199de89248b177223fe8daf9570e
blazakin <felschf05@gmail.com>
2024-06-07 13:51:48 -0700

    Added alu signal to branch module.

--- branch.sv
+++ branch.sv
@@ -18,6 +18,7 @@ module branch (
     output logic [`XLEN-1:0] alu_a,
     output logic [`XLEN-1:0] alu_b,
     output logic [2:0]       alu_op,
+    output logic             alu_signal;
     input  logic [`XLEN-1:0] alu_out,
 
     output logic             load_new_program_counter,
@@ -42,6 +43,7 @@ module branch (
             alu_b = register_data_2;
 
             alu_op = {~instruction[14], instruction[14], instruction[13]};
+            alu_signal = 0'b0;
             load_new_program_counter = |alu_out ^ instruction[12];
 
             offset = {instruction[31], instruction[7], instruction[30:25],
@@ -50,7 +52,8 @@ module branch (
         end else begin
             alu_a = `BUS_HI_Z;
             alu_b = `BUS_HI_Z;
-            alu_op = `BUS_HI_Z;
+            alu_op = 3'bzzz;
+            alu_signal = 1'bz;
             load_new_program_counter = 1'bz;
             new_program_counter = `BUS_HI_Z;
         end
