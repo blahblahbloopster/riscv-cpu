0cd75c7fe98f5729825ea8c8ee14ceef287d077d
blazakin <felschf05@gmail.com>
2024-06-07 12:44:21 -0700

    Rename alu_... files to be consistent with conventions

--- alu_reg.sv
+++ alu_reg.sv
@@ -3,14 +3,16 @@
 `define BUS_HI_Z `XLEN'hzzzzzzzz
 `define SELECT_HI_Z `REG_SELECT_LEN'bzzzzz
 
-module ALU_imm (
+module alu_reg (
     input  logic             clk,
     input  logic             enable_n,
 
     input  logic [`XLEN-1:0] instruction,
 
     output logic [`REG_SELECT_LEN-1:0] register_1,
+    output logic [`REG_SELECT_LEN-1:0] register_2,
     input  logic [`XLEN-1:0]           register_data_1,
+    input  logic [`XLEN-1:0]           register_data_2,
 
     output logic [`XLEN-1:0] alu_a,
     output logic [`XLEN-1:0] alu_b,
@@ -25,19 +27,20 @@ module ALU_imm (
     always @(posedge clk) begin
         if (!enable_n) begin
             register_1 <= instruction[12:8];
+            register_2 <= instruction[17:13];
         end else begin
             register_1 <= `SELECT_HI_Z;
+            register_2 <= `SELECT_HI_Z;
         end
     end
 
     always_comb begin
         if (!enable_n) begin
-
             alu_a = register_data_1;
-            alu_b = {5'h00000, instruction[31:20]};
+            alu_b = register_data_2;
+
             alu_op = instruction[15:13];
             alu_sig = instruction[30];
-
             output_register = instruction[12:8];
             output_register_data = alu_out;
         end else begin
