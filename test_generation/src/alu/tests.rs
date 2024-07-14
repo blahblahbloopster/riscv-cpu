df0c7a0cbe24e5aa0c713273bed7ec106e86e1ef
Quinn Yockey <qyockey@tutanota.de>
2024-07-14 00:04:58 -0700

    ALU tests passing

--- test_generation/src/alu/tests.rs
+++ test_generation/src/alu/tests.rs
@@ -18,6 +18,24 @@ macro_rules! test_add {
     };
 }
 
+macro_rules! test_sub {
+    ($num:ident, $expected:expr, $a:expr, $b:expr) => {
+        #[test]
+        fn $num() {
+            let alu = Alu::new();
+            let add_inputs = AluInputs::new(
+                false,
+                AluFunct::ADD.into(),
+                true,
+                $a,
+                $b,
+            );
+            let result = alu.get_state(add_inputs).result;
+            assert_eq!(result, Some($expected));
+        }
+    };
+}
+
 macro_rules! test_sll {
     ($num:ident, $expected:expr, $a:expr, $b:expr) => {
         #[test]
@@ -43,7 +61,25 @@ macro_rules! test_slt {
             let alu = Alu::new();
             let add_inputs = AluInputs::new(
                 false,
-                AluFunct::SLL.into(),
+                AluFunct::SLT.into(),
+                false,
+                $a,
+                $b,
+            );
+            let result = alu.get_state(add_inputs).result;
+            assert_eq!(result, Some($expected));
+        }
+    };
+}
+
+macro_rules! test_sltu {
+    ($num:ident, $expected:expr, $a:expr, $b:expr) => {
+        #[test]
+        fn $num() {
+            let alu = Alu::new();
+            let add_inputs = AluInputs::new(
+                false,
+                AluFunct::SLTU.into(),
                 false,
                 $a,
                 $b,
@@ -145,6 +181,7 @@ macro_rules! test_and {
     };
 }
 
+// ALU tests taken from https://github.com/riscv-software-src/riscv-tests/tree/197479964e99ee9a0b196c9cc1a3249812477bd3/isa/rv32ui
 
 // ADD
 
@@ -168,6 +205,27 @@ test_add!(add_14, 0x00000000, 0xffffffff, 0x00000001);
 test_add!(add_15, 0xfffffffe, 0xffffffff, 0xffffffff);
 
 test_add!(add_16, 0x80000000, 0x00000001, 0x7fffffff);
+
+// SUB
+
+test_sub!(sub_2,  0x00000000, 0x00000000, 0x00000000);
+test_sub!(sub_3,  0x00000000, 0x00000001, 0x00000001);
+test_sub!(sub_4,  0xfffffffc, 0x00000003, 0x00000007);
+
+test_sub!(sub_5,  0x00008000, 0x00000000, 0xffff8000);
+test_sub!(sub_6,  0x80000000, 0x80000000, 0x00000000);
+test_sub!(sub_7,  0x80008000, 0x80000000, 0xffff8000);
+
+test_sub!(sub_8,  0xffff8001, 0x00000000, 0x00007fff);
+test_sub!(sub_9,  0x7fffffff, 0x7fffffff, 0x00000000);
+test_sub!(sub_10, 0x7fff8000, 0x7fffffff, 0x00007fff);
+
+test_sub!(sub_11, 0x7fff8001, 0x80000000, 0x00007fff);
+test_sub!(sub_12, 0x80007fff, 0x7fffffff, 0xffff8000);
+
+test_sub!(sub_13, 0x00000001, 0x00000000, 0xffffffff);
+test_sub!(sub_14, 0xfffffffe, 0xffffffff, 0x00000001);
+test_sub!(sub_15, 0x00000000, 0xffffffff, 0xffffffff);
 
 // SLL
 
@@ -217,6 +275,28 @@ test_slt!(slt_14, 0, 0x00000000, 0xffffffff);
 test_slt!(slt_15, 1, 0xffffffff, 0x00000001);
 test_slt!(slt_16, 0, 0xffffffff, 0xffffffff);
 
+// SLTU
+
+test_sltu!(sltu_2,  0, 0x00000000, 0x00000000);
+test_sltu!(sltu_3,  0, 0x00000001, 0x00000001);
+test_sltu!(sltu_4,  1, 0x00000003, 0x00000007);
+test_sltu!(sltu_5,  0, 0x00000007, 0x00000003);
+
+test_sltu!(sltu_6,  1, 0x00000000, 0xffff8000);
+test_sltu!(sltu_7,  0, 0x80000000, 0x00000000);
+test_sltu!(sltu_8,  1, 0x80000000, 0xffff8000);
+
+test_sltu!(sltu_9,  1, 0x00000000, 0x00007fff);
+test_sltu!(sltu_10, 0, 0x7fffffff, 0x00000000);
+test_sltu!(sltu_11, 0, 0x7fffffff, 0x00007fff);
+
+test_sltu!(sltu_12, 0, 0x80000000, 0x00007fff);
+test_sltu!(sltu_13, 1, 0x7fffffff, 0xffff8000);
+
+test_sltu!(sltu_14, 1, 0x00000000, 0xffffffff);
+test_sltu!(sltu_15, 0, 0xffffffff, 0x00000001);
+test_sltu!(sltu_16, 0, 0xffffffff, 0xffffffff);
+
 // XOR
 
 test_xor!(xor_2, 0xf00ff00f, 0xff00ff00, 0x0f0f0f0f);
@@ -226,23 +306,29 @@ test_xor!(xor_5, 0x00ff00ff, 0xf00ff00f, 0xf0f0f0f0);
 
 // SRL
 
-test_srl!(srl_2,  0x80000000, 0x80000000, 0 );
-test_srl!(srl_3,  0xc0000000, 0x80000000, 1 );
-test_srl!(srl_4,  0xff000000, 0x80000000, 7 );
-test_srl!(srl_5,  0xfffe0000, 0x80000000, 14);
-test_srl!(srl_6,  0xffffffff, 0x80000001, 31);
+test_srl!(srl_2,  0xffff8000, 0xffff8000, 0 );
+test_srl!(srl_3,  0x7fffc000, 0xffff8000, 1 );
+test_srl!(srl_4,  0x01ffff00, 0xffff8000, 7 );
+test_srl!(srl_5,  0x0003fffe, 0xffff8000, 14);
+test_srl!(srl_6,  0x0001ffff, 0xffff8001, 15);
 
 test_srl!(srl_7,  0xffffffff, 0xffffffff, 0 );
-test_srl!(srl_8,  0xffffffff, 0xffffffff, 1 );
-test_srl!(srl_9,  0xffffffff, 0xffffffff, 7 );
-test_srl!(srl_10, 0xffffffff, 0xffffffff, 14);
-test_srl!(srl_11, 0xffffffff, 0xffffffff, 31);
+test_srl!(srl_8,  0x7fffffff, 0xffffffff, 1 );
+test_srl!(srl_9,  0x01ffffff, 0xffffffff, 7 );
+test_srl!(srl_10, 0x0003ffff, 0xffffffff, 14);
+test_srl!(srl_11, 0x00000001, 0xffffffff, 31);
 
 test_srl!(srl_12, 0x21212121, 0x21212121, 0 );
 test_srl!(srl_13, 0x10909090, 0x21212121, 1 );
 test_srl!(srl_14, 0x00424242, 0x21212121, 7 );
 test_srl!(srl_15, 0x00008484, 0x21212121, 14);
 test_srl!(srl_16, 0x00000000, 0x21212121, 31);
+
+test_srl!(srl_17, 0x21212121, 0x21212121, 0xffffffe0);
+test_srl!(srl_18, 0x10909090, 0x21212121, 0xffffffe1);
+test_srl!(srl_19, 0x00424242, 0x21212121, 0xffffffe7);
+test_srl!(srl_20, 0x00008484, 0x21212121, 0xffffffee);
+test_srl!(srl_21, 0x00000000, 0x21212121, 0xffffffff);
 
 // SRA
 
